3266d37886fcca833edad187825e7719bfc7a3b2
Dan Kiss <flowfast47@gmail.com>
2023-07-28 10:56:27 -0700

    add StylistsController create and index with views

--- Controllers/StylistsController.cs
+++ Controllers/StylistsController.cs
@@ -22,5 +22,20 @@ namespace HairSalon.Controllers
             List<Stylist> model = _db.Stylists.ToList();
             return View(model);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Stylist stylist)
+        {
+            _db.Stylists.Add(stylist);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //need details aka clients of one specific stylist
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Add readme to Hair Salon
 
 -include the production database for appsettings.json
+
+make url strings lowercase
