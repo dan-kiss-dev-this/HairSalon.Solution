355ca510d5b6d7a675510f4018050bae1da398ea
Dan Kiss <flowfast47@gmail.com>
2023-07-28 09:11:53 -0700

    add client and stylist models

---
+++ Models/Client.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string HairCareNotes { get; set; }
+
+        public Stylist Stylist { get; set; }
+
+        // public List<Client> Clients { get; set; }
+    }
+}
